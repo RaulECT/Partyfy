e97805c787534a8289f5ff5acfb7782167600c44
Raul Canul <eduardoct120@gmail.com>
2018-01-26 17:50:24 -0600

    First commit

---
+++ Partyfy/Constants.swift
@@ -0,0 +1,16 @@
+//
+//  Constants.swift
+//  Partyfy
+//
+//  Created by Raul  Canul on 26/01/18.
+//  Copyright © 2018 Raul  Canul. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct Colors {
+    static let baseColor = UIColor(red: 77.0/255.0, green: 145.0/255.0, blue: 217.0/255.0, alpha: 1.0)
+    static let accentColor = UIColor(red: 129.0/255.0, green: 210.0/255.0, blue: 199.0/255.0, alpha: 1.0)
+    static let whiteColor = UIColor(red: 255.0/255.0, green: 255.0/255.0, blue: 255.0/255.0, alpha:1.0)
+}
---
+++ Partyfy/ViewController.swift
@@ -0,0 +1,53 @@
+//
+//  ViewController.swift
+//  Partyfy
+//
+//  Created by Raul  Canul on 26/01/18.
+//  Copyright © 2018 Raul  Canul. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    // MARK: UI Elements
+    @IBOutlet weak var loginButton: UIButton!
+    @IBOutlet weak var resgisterButton: UIButton!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.setGradiantBackground(colorOne: Colors.baseColor, colorTwo: Colors.accentColor)
+        
+        // MARK: Buttons Configuration
+        self.loginButton.layer.borderWidth = 1.0
+        self.loginButton.layer.borderColor = Colors.whiteColor.cgColor
+        
+        self.resgisterButton.layer.borderWidth = 1.0
+        self.resgisterButton.layer.borderColor = Colors.whiteColor.cgColor
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
+extension UIView {
+    func setGradiantBackground( colorOne: UIColor, colorTwo: UIColor ) {
+        let gradiantLayer = CAGradientLayer()
+        gradiantLayer.frame = bounds
+        gradiantLayer.colors = [ colorOne.cgColor, colorTwo.cgColor ]
+        
+        gradiantLayer.locations = [ 0.0, 1.0 ]
+        
+        gradiantLayer.startPoint = CGPoint( x:0.0,y: 0.0)
+        gradiantLayer.endPoint = CGPoint(x:1.0, y:1.0)
+        
+        layer.insertSublayer(gradiantLayer, at: 0)
+    }
+}
+
